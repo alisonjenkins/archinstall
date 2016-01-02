52bf08b6bc542c0893e7c129b42c998aab62bb4d
Alan Jenkins <alan.james.jenkins@gmail.com>
2016-01-02 13:23:53 +0000

    Rename files.

--- install.sh
+++ install.sh
@@ -1,56 +1,10 @@
-#/bin/bash
+#!/bin/bash
+pacstrap /mnt base base-devel
+genfstab -P /mnt > /mnt/etc/fstab
 
-function hostname()
-{
-    echo "What do you want to name the computer?"
-    read hostname
-    echo "$hostname" > /etc/hostname
-}
+# grab other script to execute in the chroot
+wget https://raw.githubusercontent.com/demon012/archinstall/develop/install.sh -O /mnt/install.sh
+chmod +x /mnt/install.sh
 
-function timezone()
-{
-    ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
-}
-
-function enable_locales()
-{
-    LOCALES=( en_GB en_US )
-    for locale in "${LOCALES[@]}"
-    do
-        sed -i '/^#$locale /s/^#//' /etc/locale.gen
-    done
-
-    locale-gen
-
-    # main locale is the first argument to the locales array
-    echo "LANG=$LOCALES[0].UTF-8" > /etc/locale.conf
-}
-
-function set_keymap()
-{
-    echo "KEYMAP=uk" > /etc/vconsole.conf
-}
-
-function mkinitcpio()
-{
-    mkinitcpio -p linux
-}
-
-function install_bootloader()
-{
-    bootctl install --path=/boot
-}
-
-function install_packages()
-{
-    pacman -Sy \
-        vim
-}
-
-hostname
-timezone
-enable_locales
-set_keymap
-mkinitcpio
-install_bootloader
-install_packages
+# execute the script in the chroot
+arch-chroot /mnt /mnt/install.sh

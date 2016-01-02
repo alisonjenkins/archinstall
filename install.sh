dd02de64b602c20d5a3d3300bec9959424bfa35c
Alan Jenkins <alan.james.jenkins@gmail.com>
2016-01-02 13:26:04 +0000

    Change the url of the file that the script will pull for the second stage of the install.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ pacstrap /mnt base base-devel
 genfstab -P /mnt > /mnt/etc/fstab
 
 # grab other script to execute in the chroot
-wget https://raw.githubusercontent.com/demon012/archinstall/develop/install.sh -O /mnt/install.sh
+wget https://raw.githubusercontent.com/demon012/archinstall/master/install-main.sh -O /mnt/install.sh
 chmod +x /mnt/install.sh
 
 # execute the script in the chroot

f28d4638b28305866aed6775e5f7afbfc4e1cf34
Alan Jenkins <alan.jenkins@claranet.uk>
2019-09-21 22:18:12 +0100

    Fix name of playbook to run

--- arch-install.sh
+++ arch-install.sh
@@ -218,7 +218,7 @@ run_ansible() # {{{
 {
     cat <<'END' | arch-chroot /mnt su -l root
     cd /etc/ansible/
-    ansible-playbook plabooks/desktop.yml
+    ansible-playbook playbooks/desktop.yml
 END
 } # }}}
 

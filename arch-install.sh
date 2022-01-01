266f8a82e34ca3a537d8c48f7fe64a4796530b6a
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-01-01 09:00:23 +0000

    Fix issues with fold markers caused by shfmt

--- arch-install.sh
+++ arch-install.sh
@@ -22,19 +22,23 @@ get_partitions() { # {{{
 			break
 		fi
 	done
-}                 #}}}
+} #}}}
+
 get_partition() { # {{{
 	local CHILD_NUM="$1"
 	PART_NAME=$(echo "$DISK_CHILDREN" | jq -r ".[$CHILD_NUM].name")
-}                  # }}}
+} # }}}
+
 chroot_command() { # {{{
 	COMMAND="arch-chroot /mnt $1"
 	echo "Running: $COMMAND"
 	eval $COMMAND
-}                # }}}
+} # }}}
+
 install_deps() { # {{{
 	pacman -Sy --noconfirm jq lvm2 btrfs-progs dialog
-}                       # }}}
+} # }}}
+
 select_install_disk() { # {{{
 	NUM_DISKS=$(lsblk -J | jq '.[] | length')
 	DISKS=""
@@ -72,22 +76,26 @@ select_install_disk() { # {{{
 		unset SEL_DISK
 		dialog --clear
 	fi
-}                           # }}}
+} # }}}
+
 get_encryption_password() { # {{{
 	local COMMAND="dialog --stdout --passwordbox \"Please enter the password to use for disk encryption\" 8 50"
 	ENCRPYTION_PASS="$(eval $COMMAND)"
 	dialog --clear
-}                         # }}}
+} # }}}
+
 get_required_hostname() { # {{{
 	local COMMAND="dialog --stdout --inputbox \"Please enter the hostname you want to use for the system.\" 8 50"
 	REQUIRED_HOSTNAME="$(eval $COMMAND)"
 	dialog --clear
-}             # }}}
+} # }}}
+
 wipe_disk() { # {{{
 	echo "Wiping disk"
 	wipefs -a "$DISK_PATH"
 }
 # }}}
+
 partition_disk() { # {{{
 	echo "Partitioning disk: $DISK_PATH"
 	# Setup EFI and boot
@@ -98,13 +106,15 @@ partition_disk() { # {{{
 	parted -s "$DISK_PATH" "name 2 lvm"
 	parted -s "$DISK_PATH" "toggle 1 boot"
 	parted -s "$DISK_PATH" "toggle 2 lvm"
-}                     # }}}
+} # }}}
+
 format_partitions() { # {{{
 	echo "Formatting partitions"
 	get_partitions "$DISK"
 	get_partition 0
 	mkfs.vfat -F32 /dev/"$PART_NAME"
-}              # }}}
+} # }}}
+
 setup_luks() { # {{{
 	echo "Setting up encrypted partitions"
 	get_partition 1
@@ -116,7 +126,8 @@ setup_luks() { # {{{
 	lvcreate -l 100%FREE volgroup -n lvolroot
 	mkswap -L swap /dev/mapper/volgroup-lvolswap
 	mkfs.xfs -L root /dev/mapper/volgroup-lvolroot
-}                    # }}}
+} # }}}
+
 mount_partitions() { # {{{
 	echo "Mounting partitions"
 	mount /dev/mapper/volgroup-lvolroot /mnt
@@ -128,40 +139,46 @@ mount_partitions() { # {{{
 	mkdir -p /mnt/boot
 	mkdir -p /mnt/efi/EFI/arch
 	mount --bind /mnt/efi/EFI/arch /mnt/boot
-}                                # }}}
+} # }}}
+
 function find_fastest_mirror() { # {{{
 	pacman -S --noconfirm reflector pacman-contrib
 	#curl -s "https://www.archlinux.org/mirrorlist/?country=FR&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 	reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 	mkdir -p /mnt/etc/pacman.d
 	cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-}                       # }}}
+} # }}}
+
 install_base_system() { # {{{
 	echo "Installing system"
 	mkdir -p /mnt/etc/
 	genfstab -L /mnt >/mnt/etc/fstab
 	sed -i 's#/mnt/efi/EFI/arch#/efi/EFI/arch#' /mnt/etc/fstab
 	pacstrap /mnt base base-devel curl efibootmgr btrfs-progs git ansible wget ruby-shadow linux linux-headers linux-zen linux-zen-headers linux-headers nvidia-dkms virtualbox-host-dkms iwd intel-ucode lvm2 xfsprogs linux-firmware
-}                 # }}}
+} # }}}
+
 setup_locales() { # {{{
 	echo "Setting locale"
 	chroot_command "sed -i 's/#en_GB/en_GB/g' /etc/locale.gen"
 	chroot_command "sed -i 's/#en_US/en_US/g' /etc/locale.gen"
 	chroot_command "locale-gen"
 	echo 'LANG=en_GB.UTF-8' >/mnt/etc/locale.conf
-}                  # }}}
+} # }}}
+
 setup_hostname() { # {{{
 	echo "Setting hostname to $REQUIRED_HOSTNAME"
 	#chroot_command "bash echo 'test' > /etc/hostname"
 	echo "$REQUIRED_HOSTNAME" >/mnt/etc/hostname
 	chroot_command "hostnamectl set-hostname \"$REQUIRED_HOSTNAME\""
-}                   # }}}
+} # }}}
+
 create_initcpio() { # {{{
 	echo "Creating initcpio"
 	chroot_command "sed -i 's/base udev autodetect modconf block filesystems keyboard fsck/base udev encrypt autodetect modconf block lvm2 resume filesystems keyboard fsck/g' /etc/mkinitcpio.conf"
 	chroot_command "mkinitcpio -p linux"
 	chroot_command "mkinitcpio -p linux-zen"
-}                      # }}}
+} # }}}
+
 setup_systemd_boot() { # {{{
 	echo "Setting up systemd-boot"
 	get_partition 1
@@ -174,17 +191,13 @@ setup_systemd_boot() { # {{{
 	echo "initrd /EFI/arch/intel-ucode.img" >>/mnt/efi/loader/entries/arch.conf
 	echo "initrd /EFI/arch/initramfs-linux.img" >>/mnt/efi/loader/entries/arch.conf
 	echo "options cryptdevice=${LUKSUUID}:lvm root=/dev/mapper/volgroup-lvolroot resume=/dev/mapper/volgroup-lvolswap rw initrd=/EFI/arch/initramfs-linux.img button.lid_init_state=open quiet splash loglevel=3 rd.udev.log-priority=3 vt.global_cursor_default=0 vga=current i915.fastboot=1 i915.enable_gvt=1 kvm.ignore_msrs=1 intel_iommu=on iommu=pt" >>/mnt/efi/loader/entries/arch.conf
+} # }}}
 
-	echo "label Arch Linux Zen" >>/mnt/efi/loader/entries/arch-zen.conf
-	echo "initrd /EFI/arch/intel-ucode.img" >>/mnt/efi/loader/entries/arch-zen.conf
-	echo "linux /EFI/arch/vmlinuz-linux-zen" >>/mnt/efi/loader/entries/arch-zen.conf
-	echo "initrd /EFI/arch/initramfs-linux-zen.img" >>/mnt/efi/loader/entries/arch-zen.conf
-	echo "options cryptdevice=${LUKSUUID}:lvm root=/dev/mapper/volgroup-lvolroot resume=/dev/mapper/volgroup-lvolswap rw initrd=/EFI/arch/initramfs-linux-zen.img button.lid_init_state=open quiet splash loglevel=3 rd.udev.log-priority=3 vt.global_cursor_default=0 vga=current i915.fastboot=1 i915.enable_gvt=1 kvm.ignore_msrs=1 intel_iommu=on iommu=pt" >>/mnt/efi/loader/entries/arch-zen.conf
-}                    # }}}
 get_ansible_code() { # {{{
 	chroot_command "rm -Rf /etc/ansible"
 	chroot_command "git clone --depth=1 --recurse-submodules -j8 https://github.com/alanjjenkins/ansible.git /etc/ansible/"
-}               #}}}
+} #}}}
+
 run_ansible() { # {{{
 	cat <<'END' | arch-chroot /mnt su -l root
     cd /etc/ansible/
